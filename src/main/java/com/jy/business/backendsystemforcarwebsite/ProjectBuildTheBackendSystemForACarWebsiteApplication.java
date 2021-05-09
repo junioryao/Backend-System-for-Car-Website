2779fb77e25e5ddf145973b73a61b8a3efa9f0a3
junior yao <junioryao.jy@gmail.com>
2021-05-09 02:20:13 +0200

    construct first project

---
+++ src/main/java/com/jy/business/backendsystemforcarwebsite/ProjectBuildTheBackendSystemForACarWebsiteApplication.java
@@ -0,0 +1,13 @@
+package com.jy.business.backendsystemforcarwebsite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectBuildTheBackendSystemForACarWebsiteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectBuildTheBackendSystemForACarWebsiteApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jy/business/backendsystemforcarwebsite/ProjectBuildTheBackendSystemForACarWebsiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jy.business.backendsystemforcarwebsite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectBuildTheBackendSystemForACarWebsiteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
